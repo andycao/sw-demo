456ca02772236b64ac36608f719349f76e59bb1a
andy <huayu.cao@pintec.com>
2020-04-22 13:29:42 +0800

    sw

--- sw.js
+++ sw.js
@@ -2,21 +2,24 @@ self.addEventListener("install", (event) => {
     console.log("V1 installing…");
 
     // cache a cat SVG
-    event.waitUntil(caches.open("static-v1").then((cache) => cache.add("./cat.svg")));
+    event.waitUntil(
+        caches.open("static-v1").then((cache) => {
+            Promise.all(cache.add("./cat.svg"), cache.add("./dog.svg"));
+        })
+    );
 });
 
 self.addEventListener("activate", (event) => {
+    clients.claim();
     console.log("V1 now ready to handle fetches!");
 });
 
 self.addEventListener("fetch", (event) => {
     const url = new URL(event.request.url);
     console.log(event.request);
-    // serve the cat SVG from the cache if the request is
-    // same-origin and the path is '/dog.svg'
-    const reg = /dog.svg$/i;
-
-    if (url.origin == location.origin && reg.test(url.pathname)) {
-        event.respondWith(caches.match("./cat.svg"));
+    
+    // serve the cat SVG from the cache if the request is same-origin
+    if (url.origin == location.origin) {
+        event.respondWith(caches.match(url.pathname));
     }
 });
